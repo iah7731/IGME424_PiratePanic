12e2b1fe4c8415c7ca62ea78b8ca05ad0957f8a4
Ivan Hernandez <iah7731@rit.edu>
2025-10-01 23:35:07 -0400

    Pirate Sells Custom Items

---
+++ Content/NPCs/ModGlobalNPC.cs
@@ -0,0 +1,18 @@
+using Terraria;
+using Terraria.ID;
+using Terraria.ModLoader;
+using PiratePanic.Content.Items;
+
+namespace PiratePanic.Content.NPCs
+{
+    public class ExampleGlobalNPC : GlobalNPC
+    {
+        public override void ModifyShop (NPCShop shop) 	
+        {
+            if (shop.NpcType == NPCID.Pirate)
+            {
+                shop.Add(<TutorialSword>());
+            }
+        }
+    }
+}
