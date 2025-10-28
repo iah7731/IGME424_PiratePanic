782bd9b5ad578f9fe9c80c41387f17e744317542
Ivan Hernandez <iah7731@rit.edu>
2025-10-27 20:19:48 -0400

    Added Skeleton Reskins
    - Added Angry Jones (Angry Bones), Ragged Jones (Ragged Caster), and Spirit Diabolist (Diabolist).

---
+++ Content/NPCs/AngryJones.cs
@@ -0,0 +1,82 @@
+using Microsoft.Xna.Framework;
+using System;
+using System.IO;
+using Terraria;
+using Terraria.GameContent.Bestiary;
+using Terraria.GameContent.UI;
+using Terraria.ID;
+using Terraria.ModLoader;
+using Terraria.ModLoader.IO;
+using Terraria.ModLoader.Utilities;
+
+namespace PiratePanic.Content.NPCs
+{
+	public class AngryJones : ModNPC
+	{
+		public override void SetStaticDefaults() {
+			Main.npcFrameCount[Type] = Main.npcFrameCount[NPCID.AngryBones];
+
+			NPCID.Sets.NPCBestiaryDrawModifiers value = new NPCID.Sets.NPCBestiaryDrawModifiers() {
+				Velocity = 1f 
+			};
+			NPCID.Sets.NPCBestiaryDrawOffset.Add(Type, value);
+		}
+
+		public override void SetDefaults() {
+			NPC.width = 18;
+			NPC.height = 40;
+			NPC.damage = 65;
+			NPC.defense = 16;
+			NPC.lifeMax = 250;
+			NPC.HitSound = SoundID.NPCHit1;
+			NPC.DeathSound = SoundID.NPCDeath1;
+			NPC.value = 60f;
+			NPC.knockBackResist = 0.7f;
+			NPC.aiStyle = NPCAIStyleID.Fighter; 
+
+			AIType = NPCID.AngryBones; // Might end up despawning if there isn't a Pirate Invasion. Needs to be checked.
+			AnimationType = NPCID.AngryBones; 
+			Banner = Item.NPCtoBanner(NPCID.AngryBones);
+			BannerItem = Item.BannerToItem(Banner); 
+		}
+
+		public override void SetBestiary(BestiaryDatabase database, BestiaryEntry bestiaryEntry) {
+			// We can use AddRange instead of calling Add multiple times in order to add multiple items at once
+			bestiaryEntry.Info.AddRange([
+				// Sets the spawning conditions of this NPC that is listed in the bestiary.
+				BestiaryDatabaseNPCsPopulator.CommonTags.SpawnConditions.Times.NightTime,
+
+				// Sets the description of this NPC that is listed in the bestiary.
+				new FlavorTextBestiaryInfoElement("AngryBones"),
+			]);
+		}
+
+		public override void HitEffect(NPC.HitInfo hit) {
+
+			// Create gore when the NPC is killed.
+			if (Main.netMode != NetmodeID.Server && NPC.life <= 0) {
+				// Retrieve the gore types. This NPC only has shimmer variants. (6 total gores)
+				string variant = "";
+				int headGore = Mod.Find<ModGore>($"{Name}_Gore{variant}_Head").Type;
+				int armGore = Mod.Find<ModGore>($"{Name}_Gore{variant}_Arm").Type;
+				int legGore = Mod.Find<ModGore>($"{Name}_Gore{variant}_Leg").Type;
+
+				// Spawn the gores. The positions of the arms and legs are lowered for a more natural look.
+				Gore.NewGore(NPC.GetSource_Death(), NPC.position, NPC.velocity, headGore, 1f);
+				Gore.NewGore(NPC.GetSource_Death(), NPC.position + new Vector2(0, 20), NPC.velocity, armGore);
+				Gore.NewGore(NPC.GetSource_Death(), NPC.position + new Vector2(0, 20), NPC.velocity, armGore);
+				Gore.NewGore(NPC.GetSource_Death(), NPC.position + new Vector2(0, 34), NPC.velocity, legGore);
+				Gore.NewGore(NPC.GetSource_Death(), NPC.position + new Vector2(0, 34), NPC.velocity, legGore);
+			}
+		}
+
+		// public override float SpawnChance(NPCSpawnInfo spawnInfo) {
+		// 	// Can only spawn in the ExampleSurfaceBiome and if there are no other ExampleZombieThiefs
+		// 	if (spawnInfo.Player.InModBiome(ModContent.GetInstance<ExampleSurfaceBiome>()) && !NPC.AnyNPCs(Type)) {
+		// 		return SpawnCondition.OverworldNightMonster.Chance * 0.1f; // Spawn with 1/10th the chance of a regular zombie.
+		// 	}
+
+		// 	return 0f;
+		// }
+	}
+}
