c8b0f6319f1a16ff00102f9916e8cd22ee376da3
Ivan Hernandez <123189416+iah7731@users.noreply.github.com>
2025-11-03 17:42:20 -0500

    First Attempt at adding a Biome

---
+++ Content/Biomes/DaveEJonesIsland.cs
@@ -0,0 +1,41 @@
+using SubworldLibrary;
+using PiratePanic.Content.Subworlds;
+using Microsoft.Xna.Framework;
+using System;
+using Terraria;
+using Terraria.Graphics.Capture;
+using Terraria.ID;
+using Terraria.ModLoader;
+
+namespace PiratePanic.Content.Biomes
+{
+	// Shows setting up two basic biomes. For a more complicated example, please request.
+	public class ExampleSurfaceBiome : ModBiome
+	{
+		// Select all the scenery
+		public override ModWaterStyle WaterStyle => 6; 
+		public override ModSurfaceBackgroundStyle SurfaceBackgroundStyle => 4;
+		public override CaptureBiome.TileColorStyle TileColorStyle => CaptureBiome.TileColorStyle.Normal;
+
+		// Select Music
+		//public override int Music => MusicLoader.GetMusicSlot(Mod, "Assets/Music/MysteriousMystery");
+        public override int Music => 22;
+
+		// // Populate the Bestiary Filter
+		// public override string BestiaryIcon => base.BestiaryIcon;
+		// public override string BackgroundPath => base.BackgroundPath;
+		// public override Color? BackgroundColor => base.BackgroundColor;
+		// public override string MapBackground => BackgroundPath; // Re-uses Bestiary Background for Map Background
+
+		// Calculate when the biome is active.
+		public override bool IsBiomeActive(Player player) 
+        {
+			bool b1 = SubworldSystem.IsActive<DaveEJonesIsland>();
+			bool b3 = player.ZoneSkyHeight || player.ZoneRockLayerHeight;
+			return b1 && b3;
+		}
+
+		// Declare biome priority. The default is BiomeLow so this is only necessary if it needs a higher priority.
+		public override SceneEffectPriority Priority => SceneEffectPriority.BiomeLow;
+	}
+}
